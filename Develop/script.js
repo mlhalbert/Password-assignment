e6e92f143f0659e976a86504974cc5c9a5af8976
Megan Halbert <meganhalbert@Megans-MacBook-Air.local>
2022-11-10 08:57:56 -0600

    Created password generator in Javascript

---
+++ Develop/script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var passwordLength = 8;
+var password = "";
+
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+}
+
+document.getElementById("password").value = password
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
---
+++ README.md
@@ -0,0 +1,3 @@
+Link to deployed application: http://127.0.0.1:5500/Develop/index.html
+
+Description: This homework assignment was to create a password generator. The css and html were already given, as was a bit of the Javascript. The rest included specifying which characters were acceptable in a password and how long the password should be.
